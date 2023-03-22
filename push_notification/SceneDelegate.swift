177c44e2bf79c24465a00528189bc3d149f1bd5f
PrajjwalGupta <prajjwalg786@gmail.com>
2023-03-22 20:21:29 +0530

    Testing push notification using firebase

--- push_notification/SceneDelegate.swift
+++ push_notification/SceneDelegate.swift
@@ -16,7 +16,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+     
         guard let _ = (scene as? UIWindowScene) else { return }
+       // print(#function)
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -24,29 +27,44 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+      //  print(#function)
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        //print(#function)
+        showAlerts()
+        
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
+        //print(#function)
+        
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
+       // print(#function)
+     
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+       // print(#function)
+        
     }
-
-
+    func showAlerts() {
+        let alert = UIAlertController(title: "Foundry Visit App", message: "Please Visit Foundry Visit App", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { action in
+            print("Got Your Message!")
+        }))
+        window?.rootViewController?.present(alert, animated: true)
+    }
 }
 
--- push_notification/ViewController.swift
+++ push_notification/ViewController.swift
@@ -8,11 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //var window:UIWindow?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+       // showAlerts()
         // Do any additional setup after loading the view.
     }
+//    func showAlerts() {
+//        let alert = UIAlertController(title: "Foundry Visit App", message: "Please Visit Foundry Visit App", preferredStyle: .alert)
+//        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { action in
+//            print("Got Your Message!")
+//        }))
+//        window?.rootViewController?.present(alert, animated: true)
+//    }
 
 
 }
